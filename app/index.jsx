3f0da1b25deaf98a61ec5014c9847ee092eb7df7
JeffBuss <turbocapslock@gmail.com>
2017-01-26 13:23:21 -0700

    Fix routing issue

--- app/index.jsx
+++ app/index.jsx
@@ -7,6 +7,7 @@ import './styles.scss';
 import Main from './components/Main/Main';
 import Jokes from './components/Jokes/Jokes';
 import Header from './components/Header/Header';
+import Settings from './components/Settings/Settings';
 // import JokeButton from '../JokeButton/JokeButton';
 // import FavoriteButton from '../FavoriteButton/FavoriteButton';
 // import JokeCount from '../JokeCount/JokeCount';
@@ -14,12 +15,9 @@ import Header from './components/Header/Header';
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={Main} >
-      <Route path='header'>
-        <IndexRoute component={Header}></IndexRoute>
-      </Route>
-      <Route path='jokes'>
-        <IndexRoute component={Jokes}></IndexRoute>
-      </Route>
+      <IndexRoute component={Jokes} />
+      <Route path='jokes' component={Jokes} />
     </Route>
+    <Route path='settings' component={Settings} />
   </Router>
 , document.getElementById('root'));
